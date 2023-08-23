1d41eee8453bba5cd77932819fde7fd74c4070c0
Kirill Kostenkov <kiril.kostenkov@tripledotfortune.com>
2023-08-23 20:31:07 +0200

    Add negative floor finder test

--- AOC2015/AOC2015/Santa.cs
+++ AOC2015/AOC2015/Santa.cs
@@ -2,11 +2,17 @@
 public class Santa
 {
     public int CurrentFloor { get; private set; }
+    public int FirstNegativeOneFloorPosition { get; set; }
 
     public void Follow(string symbols)
     {
         var openedBracketsCount = symbols.Count(x => x.Equals('('));
         var closedBracketsCount = symbols.Count(x => x.Equals(')'));
         CurrentFloor = openedBracketsCount - closedBracketsCount;   
+    }
+
+    public void GetToFirstNegativeFloor(string symbols)
+    {
+        throw new NotImplementedException();
     }
 }
--- AOC2015/Tests/SantaTest.cs
+++ AOC2015/Tests/SantaTest.cs
@@ -21,4 +21,23 @@ public class SantaTest
         santa.Follow(symbols);
         Assert.AreEqual(santa.CurrentFloor, correctFloor);
     }
+
+    [Test]
+    [TestCase("()())", 5)]
+    public void When_GivenFloorSymbols_Should_GetRememberFirstNegativeOneFloor(string symbols, int position)
+    {
+        var santa = new Santa();
+        santa.GetToFirstNegativeFloor(symbols);
+        Assert.AreEqual(santa.FirstNegativeOneFloorPosition, position);
+    }
+    
+    // find the position of the first character
+    // that causes him to enter the basement (floor -1).
+    // The first character in the instructions has position 1, the second character has position 2, and so on.
+    //
+    //     For example:
+    //
+    // ) causes him to enter the basement at character position 1.
+    // ()()) causes him to enter the basement at character position 5.
+    // What is the position of the character that causes Santa to first enter the basement?
 }
