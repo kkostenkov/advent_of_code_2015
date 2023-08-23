2a002c6241ba76945beb1a234838548305c8f3e5
Vladimir Dukhovoi <vdukhovoi@gmail.com>
2023-08-23 21:42:52 +0300

    Second advent day complete

--- AOC2015/AOC2015/Santa.cs
+++ AOC2015/AOC2015/Santa.cs
@@ -2,7 +2,7 @@
 public class Santa
 {
     public int CurrentFloor { get; private set; }
-    public int FirstNegativeOneFloorPosition { get; private set; }
+    public int FirstNegativeOneFloorPosition { get; private set; } = Int32.MaxValue;
 
     public void Follow(string symbols)
     {
@@ -13,10 +13,31 @@ public class Santa
 
     public void GetToFirstNegativeFloor(string symbols)
     {
-        var currentPosition = 1;
+        var currentPosition = 0;
         var currentFloor = 0;
+        var hasBeenOnNegativeFloor = false; 
         foreach (var symbol in symbols) {
-            var moveDirection = 0;
+            if (hasBeenOnNegativeFloor)
+            {
+                break;
+            }
+
+            switch (symbol)
+            {
+                case '(':
+                    currentFloor++;
+                    break;
+                case ')':
+                    currentFloor--;
+                    break;
+            }
+
+            currentPosition++;
+            if (currentFloor == -1)
+            {
+                hasBeenOnNegativeFloor = true;
+                FirstNegativeOneFloorPosition = currentPosition;
+            }
         }
     }
 }
