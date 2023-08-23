8435092b487e67cf350395e678df520cc00775ef
Kirill Kostenkov <kiril.kostenkov@tripledotfortune.com>
2023-08-23 19:48:19 +0200

    Add test project

---
+++ AOC2015/Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using AOC2015;
+
+namespace Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var a = new Class1();
+        Assert.Pass();
+    }
+}
